87d3adbd25612594ca7d25111032359773d5c34c
Mikael <mikaelkellerfox@gmail.com>
2021-09-22 12:54:51 -0300

    ultima alteração

---
+++ Teste.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.listen(f8080,function(error){
+    if(error){console.log("Erro ao Iniciar Servidor")} else{console.log("--- Servidor Iniciado !! ---")}
+});
+
+
+app.get("/", function(req, res){res.send("Bem vindo")});
